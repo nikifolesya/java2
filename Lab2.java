42432817ba07addb0a8310bd717b8cfa484314f0
Olesya Nikiforova <olesya.nikiforova21@yandex.ru>
2022-11-02 10:58:45 +0300

    final

--- Lab2.java
+++ Lab2.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 class Lab2 {
     public static void main(String[] args) {
-        Point3d a = new Point3d(1, 2, 3);
+        Scanner sc = new Scanner(System.in);
+        double newArr[] = new double[9];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = sc.nextDouble();
+        }
+        Point3d a = new Point3d(newArr[0], newArr[1], newArr[2]);
+        Point3d b = new Point3d(newArr[3], newArr[4], newArr[5]);
+        Point3d c = new Point3d(newArr[6], newArr[7], newArr[8]);
+        /*Point3d a = new Point3d(1, 2, 3);
         Point3d b = new Point3d(2, 4, 5);
-        Point3d c = new Point3d(3, 5, 6);
+        Point3d c = new Point3d(3, 5, 6);*/
         //System.out.println(a.distanceTo(b));   
         System.out.println(computeArea(a, b, c));    
     }
+
     public static double computeArea(Point3d a, Point3d b, Point3d c) {
+       
         double ab = a.distanceTo(b);
         double bc = b.distanceTo(c);
         double ac = a.distanceTo(c);
         double p = 0.5 * (ab + bc + ac);
-        return Math.sqrt(p*(p-ab)*(p-bc)*(p-ac));
+        return Math.ceil(Math.sqrt(p*(p-ab)*(p-bc)*(p-ac)) * Math.pow(10, 2)) / Math.pow(10, 2);
     }
+
 }
--- Point3d.java
+++ Point3d.java
@@ -18,8 +18,23 @@ public class Point3d extends Point2d{
         zCoord = val;
     }
 
+
     public double distanceTo (Point3d b) {
         return Math.sqrt((this.getX() - b.getX())*(this.getX() - b.getX()) + (this.getY() - b.getY())*(this.getY() - b.getY()) + (this.getZ() - b.getZ())*(this.getZ() - b.getZ()));
     }
 
+    public boolean equalsPoint3d(Point3d b) {
+        if (b.getX() != this.getX())
+            return false;
+        if (b.getY() != this.getY())
+            return false;
+        if (b.getZ() != this.getZ())
+            return false;
+        return true;
+    }
+
+    public static boolean isTriangle(Point3d a, Point3d b, Point3d c) {
+        return (a.equalsPoint3d(b) || a.equalsPoint3d(c)
+                || b.equalsPoint3d(c)) ? false : true;
+    }
 }
